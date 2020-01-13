6c5ed5cca3c98fce8431ef18c724ba5e8e0fb6d9
pippo98s <filippo5scarcella@gmail.com>
2020-01-13 18:38:36 +0100

    footer completato

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,25 @@
+<footer>
+  <h2>Logo</h2>
+  <div class="list">
+    <ul id="customers">
+    <li><h3>Customers</h3></li>
+    <li>Lorem</li>
+    <li>Lorem</li>
+    <li>Lorem</li>
+    <li>Lorem</li>
+    <li>Lorem</li>
+    </ul>
+    <ul id="company">
+      <li><h3>Company</h3></li>
+      <li>Lorem</li>
+      <li>Lorem</li>
+      <li>Lorem</li>
+      <li>Lorem</li>
+    </ul>
+    <ul class="contacts">
+      <li><h3>Conctacts</h3></li>
+      <li>Lorem</li>
+      <li>Lorem</li>
+    </ul>
+  </div>
+</footer>
